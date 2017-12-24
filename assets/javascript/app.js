20795b58ca1f07c2d4d6fcbd20508e98463bad07
Rommel Fernando <rommelfernando@Rommels-MBP.hsd1.ca.comcast.net>
2017-12-24 14:42:02 -0800

    created object array on app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,45 @@
+//create an object array that includes all questions
+
+var questions = [{
+    question: "What year did Beverly Hills Cop get released?",
+    answerList: ["1982", "1989", "1984", "1986"],
+    rightAnswer: 2 },
+{
+    question: "How many Friday the 13th movies are there?",
+    answerList: ["11", "9", "10", "8"],
+    rightAnswer: 0 },
+{
+    question: "What year did the original A Nightmare On Elm Street come out?",
+    answerList: ["1982", "1980", "1984", "1986"],
+    rightAnswer: 2 },
+{
+    question: "WHo is the main characters in Little Shop of Horrors?",
+    answerList: ["Rick Moranis", "Ellen Greene", "Vincent Gardenia", "Steve Martin", "All of the above"],
+    rightAnswer: 4 },
+{
+    question: "In THe Breakfast Club, how many Saturday detentions did John Bender get?",
+    answerList: ["8", "6", "7", "4"],
+    rightAnswer: 0 },
+{
+    question: "Did Back To the Future part 2 come out in the 80's?",
+    answerList: ["Yes", "No"],
+    rightAnswer: 0 },
+{
+    question: "Is the Outsiders based on a book?",
+    answerList: ["Yes", "No"],
+    rightAnswer: 0 },
+{
+    question: "Who directed Karate Kid?",
+    answerList: ["John G. Avildsen", "Steven Speilberg", "John Carpenter"],
+    rightAnswer: 0 },
+{
+    question: "Aresnio Hall is Coming to America",
+    answerList: ["True", "False"],
+    rightAnswer: 0 },
+{
+    question: "Who voices Gizmo in Gremlins?",
+    answerList: ["Howie Mandel", "No one, its computer made", "Christopher Lloyd"],
+    rightAnswer: 0 
+}];
+
+
